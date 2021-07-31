cae433796a745d91de1f3e31f810fa037a85b1e4
moxrbe <one393752@gmail.com>
2021-07-31 10:58:18 +0500

    add components

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,23 @@
-const mainMenu = document.querySelector('.header__nav-list');
-const openMenu = document.querySelector('.nav__menu');
-const openIcon = document.querySelector('.nav__line');
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
 
 
 
 
 
 
-openMenu.addEventListener('click', () => {
-    mainMenu.classList.toggle('active')
-    openMenu.classList.toggle('active')
-    openIcon.classList.toggle('active')
-    
-});
 
 
 
